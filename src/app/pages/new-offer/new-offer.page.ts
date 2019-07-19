ee5a1df19f942dfbf6cf4373a91a983e0ec7477d
Justin Granda <justingranda@gmail.com>
2019-07-19 16:09:25 +0800

    new offer input validation

--- src/app/pages/new-offer/new-offer.page.ts
+++ src/app/pages/new-offer/new-offer.page.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
   styleUrls: ['./new-offer.page.scss'],
 })
 export class NewOfferPage implements OnInit {
+  offer: string;
+  provider : string;
+  type : string;
+  location: string;
+  description: string;
+  price: number;
 
-  constructor() { }
+  constructor(public alertController: AlertController) { 
+    this.offer="";
+    this.provider="";
+    this.type="";
+    this.location="";
+    this.description="";
+    this.price= 0;
+  }
+
+  async presentAlert() {
+    if(this.offer.trim().length <= 0 || this.provider.trim().length <= 0 || this.location.trim().length <= 0) {
+      const alert = await this.alertController.create({
+        header: 'Invalid Input',
+        message: 'Please fill in all missing fields.',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
+  }
 
   ngOnInit() {
   }
